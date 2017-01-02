359426ff6b40a5828f0d7d965445df535ee36e8c
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-02 20:19:46 +0900

    add Graph.swift

--- swift2_test/LogChecker/LogChecker/ViewController.swift
+++ swift2_test/LogChecker/LogChecker/ViewController.swift
@@ -23,5 +23,22 @@ class ViewController: UIViewController {
     @IBAction func PushNext(sender: AnyObject) {
     }
 
+
+    
+    class ViewController: UIViewController {
+        
+        @IBOutlet weak var scview: UIScrollView! //storyboardでスクロールビューを配置しているので接続
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            let graphview = Graph() //グラフを表示するクラス
+            scview.addSubview(graphview) //グラフをスクロールビューに配置
+            graphview.drawLineGraph() //グラフ描画開始
+            
+            scview.contentSize = CGSize(width:graphview.checkWidth()+20, height:graphview.checkHeight()) //スクロールビュー内のコンテンツサイズ設定
+        }
+        
+    }
 }
 
