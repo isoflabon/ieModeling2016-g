5cd50c52c9da77577fa20fbd5dfaab51e697e792
e155722 <e155722@ie.u-ryukyu.ac.jp>
2017-01-05 11:13:40 +0900

    2017_1/5_ソースコード
    光センサーの値をmilkcocoaに送信するプログラムです。（改良の余地有り）

--- Arduino_S/famima/famima.ino
+++ Arduino_S/famima/famima.ino
@@ -6,6 +6,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
   tone(12,349,BEAT);
   delay(BEAT/2);
   tone(12,294,BEAT);
@@ -28,6 +31,8 @@ tone(12,440,BEAT);
   delay(BEAT/2);
 tone(12,294,BEAT);
   delay(BEAT/2);
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
   delay(3000);
   
 }
